e063e21d10bd12bfa204c6c33bb1726ebfd053de
LAKSHAY PUNIA <148634500+Lakshay-punia@users.noreply.github.com>
2025-08-30 22:31:56 +0530

    first commit Initial setup done

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import './global.css'
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-center text-primary font-quicksand-bold">Welcome to my react native application</Text>
+    </View>
+  );
+}
