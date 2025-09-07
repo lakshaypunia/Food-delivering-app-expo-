02ec6a2ee6044c0be4d81b53adf068c7cb6833a6
LAKSHAY PUNIA <148634500+Lakshay-punia@users.noreply.github.com>
2025-09-07 13:50:35 +0530

    added auth screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,17 +1,25 @@
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { images } from "@/constants";
 import { Slot } from "expo-router";
-import { Text } from "react-native";
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function _Layout(){
 
     return (
-        <SafeAreaView>
-            <Text>
-                Auth Layout
-            </Text>
-            <Slot />
-        </SafeAreaView>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding" : "height"}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{height : Dimensions.get('screen').height /2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch" />
+                    <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10" />
+                </View>
+
+                <Slot />
+
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,18 +1,73 @@
-import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
+import { useState } from "react";
+import { Alert, Button, Text, View } from "react-native";
 
 
 
 
-export default function SingUp(){
+ const SingUp = () =>{
 
+    const[isSubmitting,setIsSubmitting] = useState(false)
+    const[form,setForm] = useState({name: '',email : '', password : ''})
+
+    const submit = async() => {
+        if(!form.email || !form.password || !form.name) {
+            Alert.alert('Error',"Please enter valid email address and password")
+            return
+            }
+        setIsSubmitting(true)
+
+        try{
+            Alert.alert("Success","User signed up succesfully")
+            router.replace("/")
+        }catch(error : any){
+            Alert.alert("Error",error.message)
+        }finally{
+            setIsSubmitting(false)
+        }
+    }
 
     return (
-        <View >
-            <Text>SignUp</Text>
-            <Button title="Sign In" onPress={() => {
-                router.push('/sign-in')
-            }}/>
+        <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+            <CustomInput 
+                    placeholder="Enter your full name"
+                    value={form.name}
+                    onChangeText={(text) => {setForm((prev) => ({...prev, name : text}))}}
+                    label="Full name"
+                />
+            <CustomInput 
+                    placeholder="Enter your email"
+                    value={form.email}
+                    onChangeText={(text) => {setForm((prev) => ({...prev, email : text}))}}
+                    label="Email"
+                    keyboardType="email-address"
+                />
+            <CustomInput 
+                    placeholder="Enter your password"
+                    value={form.password}
+                    onChangeText={(text) => {setForm((prev) => ({...prev, password : text}))}}
+                    label="Password"
+                    secureTextEntry={true}
+                />
+            <CustomButton
+            title="Sign Up" 
+            isLoading={isSubmitting}
+            onPress={submit}
+            />
+
+<View className="flex justify-center mt-5 flex-row gap-2">
+                <Text className="base-regular text-gray-100">
+                    Already have an account?
+                </Text>
+                <Link href={"/sign-in"} className="base-bold text-primary">
+                    Sign In
+                </Link>
+            </View>
         </View>
     )
 }
+
+
+export default SingUp
